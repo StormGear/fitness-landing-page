d8a328a87d7b4c96f2c81804af8fb97d0fcdc735
StormGear <papakofiboahen@gmail.com>
2025-02-10 14:50:56 +0000

    added flex

--- src/components/Partnerships.jsx
+++ src/components/Partnerships.jsx
@@ -22,49 +22,53 @@ const Partnerships = () => {
           </div>
       </div>
       <div>
-      <div className='p-6 flex flex-col  italic text-gray-400 text-center'>
-              <p className='underline'>Intuitive Global Consulting &</p>
-              <p>&nbsp;&nbsp;&nbsp;<span className="underline">The Kyra Company</span></p>
-      </div>
-      <div className='flex text-center justify-center'>
-       <img src={logo3} alt="" className='h-24 w-24 mx-4'/>
-        <img src={logo2} alt="" className='h-20 w-24 '/>
-      </div>
-      <div className='mt-6 p-6'>
-        <ul>
-            <li className='flex place-items-baseline mb-4'> 
-                 <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'> 
-                    <FaHospital className='' /> 
-                </div>   
-                Healthcare Organizations
-            </li>
-            <li className='flex place-items-baseline mb-4'> 
-                <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'> 
-                    <GoLaw className='' /> 
-                </div>
-                Government Agencies
-            </li>
-            <li className='flex place-items-baseline mb-4'> 
-                <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'> 
-                    <IoSchoolSharp className='' /> 
-                </div>
-                School Districts
-            </li>
-            <li className='flex place-items-baseline mb-4'> 
-                <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'> 
-                    <RiCommunityFill className='' /> 
-                </div>
-                Municipalities
-            </li>
-            <li className='flex place-items-baseline mb-4'> 
-                <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'> 
-                    <BsSuitcaseLgFill className='' /> 
-                </div>
-                Nonprofit Organizations <br/>
-                Comprehensive Service <br/>
-                Categories
-            </li>
-        </ul>
+      <div className="md:flex justify-around">
+          <div>
+              <div className='p-6 flex flex-col  italic text-gray-400 text-center'>
+                      <p className='underline'>Intuitive Global Consulting &</p>
+                      <p>&nbsp;&nbsp;&nbsp;<span className="underline">The Kyra Company</span></p>
+              </div>
+              <div className='flex text-center justify-center'>
+               <img src={logo3} alt="" className='h-24 w-24 mx-4'/>
+                <img src={logo2} alt="" className='h-20 w-24 '/>
+              </div>
+          </div>
+          <div className='mt-6 p-6'>
+            <ul>
+                <li className='flex place-items-baseline mb-4'>
+                     <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'>
+                        <FaHospital className='' />
+                    </div>
+                    Healthcare Organizations
+                </li>
+                <li className='flex place-items-baseline mb-4'>
+                    <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'>
+                        <GoLaw className='' />
+                    </div>
+                    Government Agencies
+                </li>
+                <li className='flex place-items-baseline mb-4'>
+                    <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'>
+                        <IoSchoolSharp className='' />
+                    </div>
+                    School Districts
+                </li>
+                <li className='flex place-items-baseline mb-4'>
+                    <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'>
+                        <RiCommunityFill className='' />
+                    </div>
+                    Municipalities
+                </li>
+                <li className='flex place-items-baseline mb-4'>
+                    <div className='rounded-full bg-[#ff3130] h-8 w-8 flex items-center justify-center mr-2'>
+                        <BsSuitcaseLgFill className='' />
+                    </div>
+                    Nonprofit Organizations <br/>
+                    Comprehensive Service <br/>
+                    Categories
+                </li>
+            </ul>
+          </div>
       </div>
       <div className="flex justify-end">
           <div className='border border-r-0 rounded-l-4xl border-t-black border-b-black text-right p-3 w-full md:w-1/2'>
