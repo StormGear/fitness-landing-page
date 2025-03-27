c4b6dff7470bd20c02ea0e300864aaea02d5c9ef
StormGear <papakofiboahen@gmail.com>
2025-03-27 12:40:37 +0000

    adding social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ import { TbWorldWww } from "react-icons/tb";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import youtube from '../assets/youtube.svg';
+import facebook from '../assets/facebook.svg';
+import instagram from '../assets/instagram.svg';
+import tiktok from '../assets/tiktok.svg';
 
 const Footer = () => {
   return (
@@ -12,18 +15,40 @@ const Footer = () => {
       {/* Footer */}
       <footer id="footer" className="bg-white text-black pt-40 pb-5">
         <div className="container mx-auto px-6 text-center flex flex-col md:flex-row items-center justify-center">
-          <p className='flex items-baseline mr-4'>
+          <p className='flex items-center mr-4'>
           <div className='rounded-full bg-black h-8 w-8 flex items-center justify-center mr-2'> 
           <TbWorldWww className='text-white' /> 
             </div>
-         <span className="underline cursor-pointer"><a href="https://alchemy-consulting.com/" target='_blank'>alchemy-consulting.com</a></span></p>
-         <img src={youtube} alt="" />
-          <p className='flex items-baseline mr-4'>
-          <div className='rounded-full bg-black h-8 w-8 flex items-center justify-center mr-2 my-1'> 
-                <MdEmail className='text-white' /> 
-            </div>
+         <span className="underline cursor-pointer"><a href="https://www.confihealthfit.com 
+" target='_blank'>confihealthfit.com</a></span></p>
+
+          <p className='flex items-center mr-4'>
+         <img src={facebook} alt="" className='h-7 w-7 '/>
             <span className="underline cursor-pointer">
-            <a href="mailto:arquella@alchemy-consulting.com">arquella@alchemy-consulting.com</a></span></p>
+            <a href="https://www.facebook.com/sandrabafrifah/" target='_blank'>sandrabafrifah</a></span>
+            </p>
+
+          <p className='flex items-center mr-4'>
+          <img src={youtube} alt="" className='h-7 w-7 '/>
+         
+            <span className="underline cursor-pointer">
+            <a href="https://www.youtube.com/@sandrabafrifah">@sandrabafrifah</a></span>
+            </p>
+
+          <p className='flex items-center mr-4'>
+          <img src={tiktok} alt="" className='h-7 w-7 ' />
+ 
+            <span className="underline cursor-pointer">
+            <a href="https://www.tiktok.com/@sandrabafrifah">@sandrabafrifah</a></span>
+            </p>
+
+          <p className='flex items-center mr-4'>
+          <img src={instagram} alt="" className='h-7 w-7' />
+     
+            <span className="underline cursor-pointer">
+            <a href="https://www.instagram.com/sandrabafrifah/
+">@sandrabafrifah</a></span>
+            </p>
         </div>
       </footer>
     </div>
